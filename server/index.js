600de21c67db5a4b5de96f6db938e83f73745a26
MayeeshaMusarrat <musarratmayeesha@gmail.com>
2023-11-25 17:15:08 +0600

    dotenv

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 var mysql = require('mysql2');
 const cors = require('cors');
 app.use(cors(), express.json());
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: process.env.DB_SSL,
+  host: process.env.REACT_APP_DB_HOST,
+  port: process.env.REACT_APP_DB_PORT,
+  user: process.env.REACT_APP_DB_USER,
+  password: process.env.REACT_APP_DB_PASSWORD,
+  database: process.env.REACT_APP_DB_NAME,
+  ssl: process.env.REACT_APP_DB_SSL,
 });
 
 
